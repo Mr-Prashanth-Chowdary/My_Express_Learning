dc1a22ee5dcc24b261aa500b717ceeea651c7e80
Mr-Prashanth-Chowdary <ch.prashanthchowdary007@gmail.com>
2024-01-05 23:09:59 +0530

    Feature: added basic routes

--- index.js
+++ index.js
@@ -8,6 +8,17 @@ app.get('/',(req,res)=> {
     res.send(`Welcome, The server is running on ${PORT}`)
 })
 
+app.post('/login',(req,res)=>{
+    res.send(`you are in the login route`)
+})
+
+app.post(`/singin`,(req,res)=>{
+    res.send(`you are in the signin route`)
+})
+
+app.post('/logout',(req,res)=>{
+    res.send(`you are in the logout route`)
+})
 
 
 app.listen(PORT,()=>{
