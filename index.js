a35a713a55cdfc0e1a43e7f5d5f8dc8f0e298efa
Mr-Prashanth-Chowdary <ch.prashanthchowdary007@gmail.com>
2024-01-09 22:40:17 +0530

    adding logic to the singup for db connection and pushing the data form user to db (registation)

--- index.js
+++ index.js
@@ -16,8 +16,18 @@ app.post('/login',(req,res)=>{
     res.send(`you are in the login route`)
 })
 
-app.post(`/singin`,(req,res)=>{
-    res.send(`you are in the signin route`)
+ 
+//  work on the singup 
+app.post('/singup',async(req,res)=>{
+    try{
+        const {username,password} = req.body
+        // const hashPassword = await bcrypt.hash(password,10)
+
+    }
+    catch(error){
+        res.status(500).send("there is an error while singup userdata", error)
+    }
+    
 })
 
 app.post('/logout',(req,res)=>{
